695be4ebcabbf4c2700c99d1ede1aa10837ddc96
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-11 12:15:25 +0100

    feat: List first page of characters
    - Included `baseUrl` config to use absolute imports
    - Fetch characters and list them using `getServerSideProps`

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <h1>Rick and Morty Grid</h1>
-}
-
-export default Home
---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import type { NextPage, GetServerSideProps } from "next";
+import CharacterCard from "components/CharacterCard";
+import List from "components/List";
+import type { Character, CharacterParsedResponse } from "types/character";
+import styles from "styles/index.module.css";
+
+type Props = {
+  characters: Character[];
+};
+
+const Home: NextPage<Props> = ({ characters }) => {
+  return (
+    <>
+      <h1>Rick and Morty Grid</h1>
+      <List
+        className={styles.gridOfCharacters}
+        items={characters}
+        renderItem={CharacterCard}
+      />
+    </>
+  );
+};
+
+const normalizeResponse = (response: CharacterParsedResponse): Character[] => {
+  return response.results.map((character) => ({
+    id: character.id,
+    gender: character.gender,
+    image: character.image,
+    name: character.name,
+    species: character.species,
+    status: character.status,
+    type: character.type || "Not Identified",
+  }));
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const response = await fetch(process.env.API_BASE_URL + "/character");
+  const parsedResponse: CharacterParsedResponse = await response.json();
+
+  return {
+    props: {
+      characters: normalizeResponse(parsedResponse),
+    },
+  };
+};
+
+export default Home;
