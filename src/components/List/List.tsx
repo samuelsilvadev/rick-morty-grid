fb6e093f108f1e87b6f7ac0df9716665c2cf370b
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-11 12:14:03 +0100

    refactor: Update List to accept a className

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,14 +4,16 @@ import styles from "./List.module.css";
 type ListProps<Item> = {
   items: Item[];
   renderItem: (item: Item) => ReactElement;
+  className?: string;
 };
 
 const List = <Item extends { id: string | number }>({
+  className = "",
   items,
   renderItem,
 }: ListProps<Item>) => {
   return (
-    <ul className={styles.list}>
+    <ul className={`${styles.list} ${className}`}>
       {items.map((item) => {
         return <li key={item.id}>{renderItem(item)}</li>;
       })}
