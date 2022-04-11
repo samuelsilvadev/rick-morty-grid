34a6f0fa59e409af5730a347632ff1ab380e8fee
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-11 14:31:05 +0100

    refactor: Update `CharacterCard` to receive an external className

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import type { Character } from "types/character";
 import styles from "./CharacterCard.module.css";
 
-type CharacterCardProps = Character;
+type CharacterCardProps = { className?: string } & Character;
 
 const CharacterCard = ({
   name,
@@ -11,9 +11,10 @@ const CharacterCard = ({
   gender,
   status,
   type,
+  className = "",
 }: CharacterCardProps) => {
   return (
-    <article className={styles.wrapper}>
+    <article className={`${styles.wrapper} ${className}`}>
       <Image src={image} alt={name} width={200} height={200} />
       <h2 className={styles.primaryHeading}>{name}</h2>
       <h3 className={styles.secondaryHeading}>
