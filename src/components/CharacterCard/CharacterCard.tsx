9a3beb8d917fa5f5c4d6d5c22f531f50d4715370
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-11 12:13:48 +0100

    feat: Build first version of CharacterCard

---
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import type { Character } from "types/character";
+import styles from "./CharacterCard.module.css";
+
+type CharacterCardProps = Character;
+
+const CharacterCard = ({
+  name,
+  image,
+  species,
+  gender,
+  status,
+  type,
+}: CharacterCardProps) => {
+  return (
+    <article className={styles.wrapper}>
+      <Image src={image} alt={name} width={200} height={200} />
+      <h2 className={styles.primaryHeading}>{name}</h2>
+      <h3 className={styles.secondaryHeading}>
+        {species}, {gender}, {status}
+      </h3>
+      <p className={styles.additionalInfo}>Type: {type}</p>
+    </article>
+  );
+};
+
+export default CharacterCard;
