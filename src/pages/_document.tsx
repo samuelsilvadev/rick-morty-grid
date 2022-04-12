f0cf9e405ab57ceaf77c3ffe4015c3909ec8f969
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-12 11:58:47 +0100

    feat: Replace default favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
+import Favicon from "components/Favicon";
 import { Html, Head, Main, NextScript } from "next/document";
 
 function MyDocument() {
   return (
     <Html>
       <Head>
+        <Favicon />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400&display=swap"
           rel="stylesheet"
