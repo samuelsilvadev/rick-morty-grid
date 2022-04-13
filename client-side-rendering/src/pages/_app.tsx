d217c420f2bfe14b9ac37f653c7ee7c33296f75b
Samuel Silva <samuelsilvawb@gmail.com>
2022-04-13 09:43:01 +0100

    feat: Disable `refetchOnWindowFocus` and set a stale time

--- client-side-rendering/src/pages/_app.tsx
+++ client-side-rendering/src/pages/_app.tsx
@@ -5,8 +5,19 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import Layout from "components/Layout";
 import "styles/globals.css";
 
+const ONE_HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = useRef(new QueryClient());
+  const queryClient = useRef(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          staleTime: ONE_HOUR_IN_MILLISECONDS,
+        },
+      },
+    })
+  );
 
   return (
     <QueryClientProvider client={queryClient.current}>
